cd41158e262f73ef6263794477d1b2637bfb8fd3
Oliver Evans <oliverevans96@gmail.com>
2022-04-11 22:04:17 -0700

    nixfmt

--- flake.nix
+++ flake.nix
@@ -7,28 +7,21 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       projectDir = ./.;
-      overlay = self: super:
-        {
-          flower-power = with super.python.pkgs;
-            buildPythonPackage {
-              name = "flower-power";
-              src = ./.;
-              propagatedBuildInputs = [
-                requests
-              ];
-            };
-        };
-      defaultPython = pkgs.python38;
-      finalPython = defaultPython.override {
-        packageOverrides = overlay;
+      overlay = self: super: {
+        flower-power = with super.python.pkgs;
+          buildPythonPackage {
+            name = "flower-power";
+            src = ./.;
+            propagatedBuildInputs = [ requests ];
+          };
       };
+      defaultPython = pkgs.python38;
+      finalPython = defaultPython.override { packageOverrides = overlay; };
     in flake-utils.lib.eachDefaultSystem (system: {
       inherit overlay;
       defaultPackage = finalPython.pkgs.flower-power;
       devShell = pkgs.mkShell {
-        buildInputs = [
-          (finalPython.withPackages (ps: [ ps.flower-power ]))
-        ];
+        buildInputs = [ (finalPython.withPackages (ps: [ ps.flower-power ])) ];
       };
     });
 }
